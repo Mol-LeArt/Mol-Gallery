241190e706a92deedb966078a6921a981f542fdd
audsssy <stamfordh@hotmail.com>
2021-02-08 14:21:55 -0500

    Update
    - smart contract updated

--- src/pages/Galleries.js
+++ src/pages/Galleries.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+// import { Link } from 'react-router-dom'
 import { projectFirestore } from '../firebase/config'
-import ImageGrid from '../comps/ImageGrid'
+// import ImageGrid from '../comps/ImageGrid'
 import { ethers } from 'ethers'
 import ABI from '../comps/MOLGAMMA_ABI'
 
 import './Galleries.css'
 
 const Galleries = () => {
-  const [contracts, setContracts] = useState([])
+//   const [contracts, setContracts] = useState([])
+//   const [name, setName] = useState('')
+// const [symbol, setSymbol] = useState('')
+// const [uri, setUri] = useState([])
 
   // ----- Smart Contract Interaction Config
   const provider = new ethers.providers.Web3Provider(window.ethereum, 'any')
@@ -16,22 +19,56 @@ const Galleries = () => {
 
   const importGalleries = async () => {
     const contractArray = []
+    const galleries = []
     const query = await projectFirestore.collection('gallery').get()
 
     query.forEach((doc) => {
       let data = doc.data().contract
       const contract = new ethers.Contract(doc.data().contract, ABI, signer)
 
-      const tx = contract.tokenURI(1).then((uri) => {
-        // download file and get image value
-        const fileDownloadUrl = URL.createObjectURL(uri)
-        console.log(uri)
-      }).catch((e) => {
+      contract.name().then(n => {
+          console.log(n)
+
+          contract
+            .symbol()
+            .then((s) => {
+              console.log(s)
+
+                    contract
+                      .getAllTokenURI()
+                      .then((uri) => {
+                        // download file and get image value
+                        // const fileDownloadUrl = URL.createObjectURL(uri)
+                        console.log(uri)
+                        console.log("n + s + uri = ", n, s, uri)
+                        const gallery = {
+                            name: n,
+                            symbol: s, 
+                            images: uri
+                        }
+                        console.log(gallery)
+                        galleries.push(gallery)
+                        console.log(galleries)
+                      })
+                      .catch((e) => {
+                        console.log(e)
+                      })
+            })
+            .catch((e) => {
+              console.log(e)
+            })
+      }).catch(e => {
           console.log(e)
       })
+
+      
+      
+
+      
+      
       contractArray.push(data)
       console.log(contractArray)
-      setContracts(contractArray)
+    //   setContracts(contractArray)
     })
   }
 
@@ -50,14 +87,17 @@ const Galleries = () => {
   //     })
   //   }
 
+
   useEffect(() => {
     importGalleries()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
     <div>
       <h1 className='galleries-title'>Galleries</h1>
       <p className='galleries-desc'>Here are the galleries on Mol! </p>
+
       {/* <div>
         {contracts.map((contract) => (
           <div>{contract}</div>
