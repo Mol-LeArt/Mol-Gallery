29e59ea600f1f24622b8c21e1d41cb6e384f98bd
audsssy <stamfordh@hotmail.com>
2021-03-26 12:56:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
 import { ethers } from 'ethers'
 import { GlobalContext } from './GlobalContext'
 import { projectFirestore } from './firebase/config'
